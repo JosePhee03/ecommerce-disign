44e44c8e7c3f7dbed022a79f530bb3adbe864061
JosePhee03 <tyttocr@gmail.com>
2023-04-22 20:12:36 -0300

    ✅ Create FotterDev

---
+++ src/components/FooterDev.tsx
@@ -0,0 +1,18 @@
+import { useProduct } from '@/hooks'
+import './footer_dev.sass'
+
+function FooterDev() {
+  const { store } = useProduct()
+  return (
+    <footer className='footer-dev'>
+      {store.map(cart => (
+        <article>
+          <h3>{cart.cartProducts.id}</h3>
+          <h4>{cart.cartProducts.title}</h4>
+        </article>
+      ))}
+    </footer>
+  )
+}
+
+export default FooterDev
