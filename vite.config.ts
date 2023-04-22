6ee7275df2ce5511c7123db55b9eb7ea1d9c51d6
JosePhee03 <tyttocr@gmail.com>
2023-04-22 14:21:00 -0300

    ✅ Create custom paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vitest/config'
-import path from 'path'
+import * as path from 'path'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
@@ -10,8 +10,12 @@ export default defineConfig({
     environment: 'jsdom'
   },
   resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
-    }
+    alias: { 
+      find: '@', replacement: path.resolve(__dirname, './src/'),
+      components: `${path.resolve(__dirname, './src/components/')}`,
+      assets: `${path.resolve(__dirname, './src/assets')}`,
+      pages: path.resolve(__dirname, './src/pages'),
+      models: `${path.resolve(__dirname, './src/models')}`,
+    },
   },
 })
