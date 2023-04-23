82a6bc918f46f84438ac98704b9c6844604a53da
JosePhee03 <tyttocr@gmail.com>
2023-04-23 13:40:53 -0300

    ✨ Add remove fuction to StoreReducer

--- src/store/context/StoreReducer.ts
+++ src/store/context/StoreReducer.ts
@@ -1,12 +1,13 @@
 import { ShoppingCart } from '@/models'
-import { StoreActions, TypeStoreActions,  } from './models/Store.models'
-import { AddProductToStore } from './models'
+import { AddProductToStore, RemoveProductToStore, StoreActions, TypeStoreActions } from '@/store/models'
 
 function StoreReducer (state: ShoppingCart[], action: StoreActions ): ShoppingCart[] {
   switch (action.type) {
   case TypeStoreActions.ADD:
     console.log(state)
     return AddProductToStore(action.payload, state)
+  case TypeStoreActions.REMOVE:
+    return RemoveProductToStore(action.payload, state)
   default:
     return state
   }
