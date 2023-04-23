7f16ee9342bfd7453b74d1414c2a4203dcc8c0c5
JosePhee03 <tyttocr@gmail.com>
2023-04-23 13:16:48 -0300

    ✅ Add Sun icon

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -85,6 +85,27 @@ export function MoonIcon({ className }: svgProps) {
   )
 }
 
+export function SunIcon({ className }: svgProps) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      fill="none"
+      viewBox="0 0 24 24"
+      className={className}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M12 16a4 4 0 100-8 4 4 0 000 8zM12 2v2M12 20v2M4.93 4.93l1.41 1.41M17.66 17.66l1.41 1.41M2 12h2M20 12h2M6.34 17.66l-1.41 1.41M19.07 4.93l-1.41 1.41"
+      ></path>
+    </svg>
+
+  )
+}
+
 export function FilterIcon({ className }: svgProps) {
   return (
     <svg
