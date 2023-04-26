195cb864f898b2a41e20055ab2ed894affae9516
JosePhee03 <tyttocr@gmail.com>
2023-04-26 17:20:17 -0300

    🎨 Update code format

--- src/hooks/useDarkMode.ts
+++ src/hooks/useDarkMode.ts
@@ -1,12 +1,12 @@
 import { useEffect, useState } from 'react'
 
 export function useDarkMode () {
-  const [theme, setTheme] = useState<'light' | 'dark'>('light')
+  const [ theme, setTheme ] = useState<'light' | 'dark'>('light')
 
   const toggleTheme = () => setTheme(theme === 'dark' ? 'light' : 'dark')
   useEffect(() => {
     document.documentElement.setAttribute('data-theme', theme)
-  }, [theme])
+  }, [ theme ])
 
-  return {toggleTheme, theme}
+  return { toggleTheme, theme }
 }
