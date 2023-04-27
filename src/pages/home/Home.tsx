bdea0414cbd0b2b1877e430225336df140024b1a
JosePhee03 <tyttocr@gmail.com>
2023-04-27 01:32:02 -0300

    ♻️ Refactor Home

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,20 +1,14 @@
 import { FilterCont, ProductsCont, SearchCont } from './components'
 import { Header } from '@/components'
-import { useSearchProduct } from '@/hooks'
 
 function Home () {
-  const { products } = useSearchProduct()
-  useSearchProduct()
 
   return (
     <main className='home-layout'>
       <Header />
       <SearchCont />
       <FilterCont />
-      {products.length === 0
-        ? <h3>Not Fount</h3>
-        : <ProductsCont products={products} />
-      }
+      <ProductsCont />
     </main>
   )
 }
--- src/pages/home/components/ProductsCont.tsx
+++ src/pages/home/components/ProductsCont.tsx
@@ -1,40 +1,46 @@
 import { ShoppingCardIcon } from '@/components/icons'
-import { useProduct } from '@/hooks'
-import { ProductType } from '@/models'
+import { useProduct, useSearchProduct } from '@/hooks'
 
-function ProductsCont ({ products }: {products: ProductType[]}) {
+function ProductsCont () {
   const { isProductInCart, addProduct, removeProduct } = useProduct()
+  const { products } = useSearchProduct()
 
   return (
-    <section className='products-cont'>
-      {products.map(product => (
-        <article key={product.id} className='product-card bg-primary' title={product.title}>
-          <img src={product.thumbnail} alt={product.title} className='product-card__img' />
-          <header className='product-card__header'>
-            <h6 className='text-sm medium font-primary'>{product.title}</h6>
-            <p className='text-xs'>{product.category}</p>
-          </header>
-          <footer className='product-card__footer'>
-            <h3 className='text-2xl medium font-primary'>${product.price}</h3>
-            {isProductInCart(product)
-              ? <button onClick={() => removeProduct(product)} className='product-card__footer__button-in-cart'>
-                <h6 className='text-sm'>
-                  in the
-                </h6>
-                <ShoppingCardIcon className='product-card__footer__button-in-cart__icon' />
-              </button>
-              : <button onClick={() => addProduct(product)} className='product-card__footer__button'>
-                <h6 className='text-sm'>
-                  add to
-                </h6>
-                <ShoppingCardIcon className='product-card__footer__button__icon' />
-              </button>
-            }
-          </footer>
-        </article>
-      ))
+    <>
+      {products.length === 0
+        ? <h1>Not Fount</h1>
+        : <section className='products-cont'>
+          {products.map(product => (
+            <article key={product.id} className='product-card bg-primary' title={product.title}>
+              <img src={product.thumbnail} alt={product.title} className='product-card__img' />
+              <header className='product-card__header'>
+                <h6 className='text-sm medium font-primary'>{product.title}</h6>
+                <p className='text-xs'>{product.category}</p>
+              </header>
+              <footer className='product-card__footer'>
+                <h3 className='text-2xl medium font-primary'>${product.price}</h3>
+                {isProductInCart(product)
+                  ? <button onClick={() => removeProduct(product)} className='product-card__footer__button-in-cart'>
+                    <h6 className='text-sm'>
+                    in the
+                    </h6>
+                    <ShoppingCardIcon className='product-card__footer__button-in-cart__icon' />
+                  </button>
+                  : <button onClick={() => addProduct(product)} className='product-card__footer__button'>
+                    <h6 className='text-sm'>
+                    add to
+                    </h6>
+                    <ShoppingCardIcon className='product-card__footer__button__icon' />
+                  </button>
+                }
+              </footer>
+            </article>
+          ))
+          }
+        </section>
+
       }
-    </section>
+    </>
   )
 }
 
