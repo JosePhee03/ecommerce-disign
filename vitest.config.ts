0dddc6f1387baaaa838c5339b3ccea59bc029622
JosePhee03 <tyttocr@gmail.com>
2023-04-27 00:33:12 -0300

    🚚 Rename vite.config to vitest.config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,12 +1,19 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 import * as path from 'path'
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [ react() ],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  },
   resolve: {
-    alias: { 
+    alias: {
       '@': path.resolve(__dirname, './src')
     },
   },
