927064150e992f33c6538087e1b91d2279244388
JosePhee03 <tyttocr@gmail.com>
2023-04-30 17:20:22 -0300

    ♻️ Refactor products cont

--- src/pages/home/components/products_cont/ProductsCont.tsx
+++ src/pages/home/components/products_cont/ProductsCont.tsx
@@ -1,46 +1,16 @@
-import { ShoppingCardIcon } from '@/components/icons'
-import { useProductsCart, useSearchProduct } from '@/hooks'
+import {  useSearchProduct } from '@/hooks'
+import { ProductCard } from './product_cart'
 
 function ProductsCont () {
-  const { isProductInCart, addProduct, removeProduct } = useProductsCart()
-  const { products } = useSearchProduct()
+  const { products, isLoanding, isError } = useSearchProduct()
 
   return (
-    <>
-      {products.length === 0
-        ? <h1>Not Fount</h1>
-        : <section className='products-cont'>
-          {products.map(product => (
-            <article key={product.id} className='product-card bg-primary' title={product.title}>
-              <img src={product.thumbnail} alt={product.title} className='product-card__img' />
-              <header className='product-card__header'>
-                <h6 className='text-sm medium font-primary'>{product.title}</h6>
-                <p className='text-xs'>{product.category}</p>
-              </header>
-              <footer className='product-card__footer'>
-                <h3 className='text-2xl medium font-primary'>${product.price}</h3>
-                {isProductInCart(product)
-                  ? <button onClick={() => removeProduct(product)} className='product-card__footer__button-in-cart'>
-                    <h6 className='text-sm'>
-                    in the
-                    </h6>
-                    <ShoppingCardIcon className='product-card__footer__button-in-cart__icon' />
-                  </button>
-                  : <button onClick={() => addProduct(product)} className='product-card__footer__button'>
-                    <h6 className='text-sm'>
-                    add to
-                    </h6>
-                    <ShoppingCardIcon className='product-card__footer__button__icon' />
-                  </button>
-                }
-              </footer>
-            </article>
-          ))
-          }
-        </section>
-
-      }
-    </>
+    <section className='products-cont'>
+      {isLoanding && !isError && <h1 className='tittle-text'>Loading...</h1>}
+      {isError && !isLoanding && <h1 className='tittle-text'>Error conection</h1>}
+      {!isError && !isLoanding && products.length === 0 && <h1 className='tittle-text'>Producto no Encontrado</h1>}
+      {!isError && !isLoanding && products.length !== 0 && <ProductCard products={products} />}
+    </section>
   )
 }
 
