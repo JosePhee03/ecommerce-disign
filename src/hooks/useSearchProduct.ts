9d844fb611a47de10fc3a3f90b50dc2611f00cff
JosePhee03 <tyttocr@gmail.com>
2023-04-30 17:19:00 -0300

    ♻️ Refactor useSearchProduct.ts

--- src/hooks/useSearchProduct.ts
+++ src/hooks/useSearchProduct.ts
@@ -4,54 +4,48 @@ import { useParams, useSearchParams } from 'react-router-dom'
 import { ProductType, QueryType, SearchProductType } from '@/models'
 import { getProducts } from '@/services'
 
+const SearchProductByQuery = ({ products, query }:{products: ProductType[], query: SearchProductType['query']}) => {
+  if (query === null) return products
+  const newProducts = [ ...products ]
+  const lowerCaseQuery = query.toLowerCase()
+  const FilterProductByTitle = newProducts.filter(p => p.title.toLowerCase().includes(lowerCaseQuery))
+  return FilterProductByTitle
+}
+
 function useSearchProduct () {
   const [ products, setProducts ] = useState<ProductType[]>([])
+  const [ isLoanding, setIsLoanding ] = useState<boolean>(true)
+  const [ isError, setIsError ] = useState<boolean>(false)
+  const [ page, setPage ] = useState<number>(0)
 
   const [ searchParam, setSearchParam ] = useSearchParams()
   const params = useParams<Pick<SearchProductType, 'category'>>()
 
-  const setQueryParams = (query: SearchProductType['query']) => {
-    if (query === null) return setSearchParam()
+  const setQueryParams = (query: string) => {
+    if (query === '' || query === null) return setSearchParam()
     setSearchParam({ [QueryType.query]: query })
   }
 
-  const searchProduct = ({ category, query }: SearchProductType)  => {
-    const newProducts = [ ...products ]
-    const productsByCategory = SearchProductByCategory(newProducts, category)
-    const productsByTitle = SearchProductByTitle(productsByCategory, query)
-    return productsByTitle
-  }
-
-  const SearchProductByTitle = (products: ProductType[], query: SearchProductType['query']) => {
-    if (query === null) return products
-    const lowerCaseData = query.toLowerCase()
-    const FilterProductByTitle = products.filter(p => p.title.toLowerCase().includes(lowerCaseData))
-    return FilterProductByTitle
-  }
-
-  const SearchProductByCategory = (products: ProductType[], category: SearchProductType['category']) => {
-    if (category === undefined) return products
-    const FilterProductByCategory = products.filter(product => product.category === category)
-    return FilterProductByCategory
-  }
-
   useEffect(() => {
-    const showNewProducts = searchProduct({
-      category: params.category,
-      query: searchParam.get(QueryType.query)
-    })
-    setProducts(showNewProducts)
+    console.log(searchParam.get(QueryType.query))
+    setIsLoanding(true)
+    getProducts(params.category, page)
+      .then(resProducts => {
+        const showNewProducts = SearchProductByQuery({
+          products: resProducts,
+          query: searchParam.get(QueryType.query)
+        })
+        setProducts(showNewProducts)
+        setIsLoanding(false)
+      }). catch(e => {
+        console.log('error', e)
+        setIsError(true)
+        setIsLoanding(false)
+      })
+
   }, [ params ])
 
-  useEffect(() => {
-    getProducts(params.category)
-      .then(resProducts => setProducts(resProducts))
-
-    console.log('/home')
-  }, [])
-
-  return { products, setQueryParams }
-
+  return { products, setQueryParams, isLoanding, isError, setPage }
 }
 
 export default useSearchProduct
