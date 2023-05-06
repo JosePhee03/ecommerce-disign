22f14db912f73e426d5fa39b0ec467de02869dc4
JosePhee03 <tyttocr@gmail.com>
2023-05-05 21:02:26 -0300

    ✅ Add form.reset() to searchCont.tsx

--- src/pages/home/components/search_cont/SearchCont.tsx
+++ src/pages/home/components/search_cont/SearchCont.tsx
@@ -8,14 +8,17 @@ function SearchCont () {
 
   const handleOnSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault()
-    const formData = new FormData(event.target as HTMLFormElement)
-    const { search: data } = Object.fromEntries(formData)
+    const form = event.currentTarget
+    console.log(form)
+    const SearchData  = new FormData(form)
+    const { search: data } = Object.fromEntries(SearchData)
     setQueryParams(data as string)
+    form.reset()
   }
 
   return useMemo(() => (
     <section className='search-cont'>
-      <form className='search-cont__form' onSubmit={handleOnSubmit}>
+      <form className='search-cont__form' onSubmit={(event) => handleOnSubmit(event)}>
         <input type="text" name="search" className='search-cont__form__input bg-primary font-primary' placeholder='Smartphone, Laptop...' />
         <button className='search-cont__form__button bg-primary'>
           <SearchIcon className='icon search-cont__form__button__icon' />
