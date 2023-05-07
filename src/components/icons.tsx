ecfb51980dfafec3021eb44b5e003c09bc241b8a
JosePhee03 <tyttocr@gmail.com>
2023-05-07 00:11:53 -0300

    ✅ Add trash icon

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -271,3 +271,22 @@ export function StartIcon ({ className }: SvgProps) {
     </svg>
   )
 }
+
+export function TrashIcon ({ className }: SvgProps) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+
+      fill="none"
+      stroke="currentColor"
+      className={className}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      viewBox="0 0 24 24"
+    >
+      <path d="M3 6h18M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2"></path>
+      <path d="M10 11L10 17"></path>
+      <path d="M14 11L14 17"></path>
+    </svg>
+  )
+}
