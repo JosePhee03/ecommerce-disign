26695321b5faeed5c41295ffa6f940848e32c7bf
JosePhee03 <tyttocr@gmail.com>
2023-05-09 22:28:15 -0300

    ✅ Create product and store mock

---
+++ src/mocks/Product.mock.ts
@@ -0,0 +1,20 @@
+import { ProductType } from '@/models'
+
+export const PRODUCT_MOCK: ProductType = {
+  id: 1,
+  title: 'iPhone 9',
+  description: 'An apple mobile which is nothing like apple',
+  price: 549,
+  rating: 4.69,
+  stock: 94,
+  brand: 'Apple',
+  category: 'smartphones',
+  images: [
+    'https://i.dummyjson.com/data/products/1/thumbnail.jpg',
+    'https://i.dummyjson.com/data/products/1/1.jpg',
+    'https://i.dummyjson.com/data/products/1/2.jpg',
+    'https://i.dummyjson.com/data/products/1/3.jpg',
+    'https://i.dummyjson.com/data/products/1/4.jpg',
+    'https://i.dummyjson.com/data/products/1/thumbnail.jpg'
+  ]
+}
---
+++ src/mocks/Store.mock.ts
@@ -0,0 +1,62 @@
+import { ShoppingCart } from '@/models'
+
+export const STORE_MOCK: ShoppingCart[] = [
+  {
+    'cartProducts': {
+      'id': 4,
+      'title': 'OPPOF19',
+      'description': 'OPPO F19 is officially announced on April 2021.',
+      'price': 280,
+      'brand': 'OPPO',
+      'category': 'smartphones',
+      'rating': 4.3,
+      'stock': 123,
+      'images': [
+        'https://i.dummyjson.com/data/products/4/thumbnail.jpg',
+        'https://i.dummyjson.com/data/products/4/1.jpg',
+        'https://i.dummyjson.com/data/products/4/2.jpg',
+        'https://i.dummyjson.com/data/products/4/3.jpg',
+        'https://i.dummyjson.com/data/products/4/4.jpg',
+        'https://i.dummyjson.com/data/products/4/thumbnail.jpg'
+      ]
+    },
+    'amount': 6
+  },
+  {
+    'cartProducts': {
+      'id': 3,
+      'title': 'Samsung Universe 9',
+      'description': 'Samsung\'s new variant which goes beyond Galaxy to the Universe',
+      'price': 1249,
+      'brand': 'Samsung',
+      'category': 'smartphones',
+      'rating': 4.09,
+      'stock': 36,
+      'images': [
+        'https://i.dummyjson.com/data/products/3/thumbnail.jpg',
+        'https://i.dummyjson.com/data/products/3/1.jpg'
+      ]
+    },
+    'amount': 8
+  },
+  {
+    'cartProducts': {
+      'id': 2,
+      'title': 'iPhone X',
+      'description': 'SIM-Free, Model A19211 6.5-inch Super Retina HD display with OLED technology A12 Bionic chip with ...',
+      'price': 899,
+      'brand': 'Apple',
+      'category': 'smartphones',
+      'rating': 4.44,
+      'stock': 34,
+      'images': [
+        'https://i.dummyjson.com/data/products/2/thumbnail.jpg',
+        'https://i.dummyjson.com/data/products/2/1.jpg',
+        'https://i.dummyjson.com/data/products/2/2.jpg',
+        'https://i.dummyjson.com/data/products/2/3.jpg',
+        'https://i.dummyjson.com/data/products/2/thumbnail.jpg'
+      ]
+    },
+    'amount': 9
+  }
+]
