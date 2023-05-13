b07c0b8db843909c8aae7ea3c2f74e53029c28e0
JosePhee03 <tyttocr@gmail.com>
2023-05-13 13:11:56 -0300

    ♻️ Refactor ProductCont

--- src/pages/home/components/products_cont/ProductsCont.tsx
+++ src/pages/home/components/products_cont/ProductsCont.tsx
@@ -1,6 +1,6 @@
+import { BanIcon, RotateIcon } from '@/components'
 import { useSearchProduct } from '@/hooks'
 import { ProductCard } from './product_cart'
-import { BanIcon, RotateIcon } from '@/components'
 
 function ProductsCont () {
   const { products, isLoanding, isError } = useSearchProduct()
@@ -12,7 +12,10 @@ function ProductsCont () {
       {!isError && !isLoanding && products.length === 0 && <div className='center'><h1 className='title-text font-primary'>No results found</h1><BanIcon className='icon-error' /><button className='button-error' onClick={() => history.back()}>Back</button></div>}
       {!isError && !isLoanding && products.length !== 0 &&
        <section className='products-cont'>
-         <ProductCard products={products} />
+         {products.map(product => (
+           <ProductCard product={product}/>
+         ))
+         }
        </section>
       }
     </>
--- src/pages/home/components/products_cont/product_cart/ProductCard.tsx
+++ src/pages/home/components/products_cont/product_cart/ProductCard.tsx
@@ -1,38 +1,37 @@
 import { Link } from 'react-router-dom'
 
 import { ButtonCart } from '@/components'
-import { useProductsCart } from '@/hooks'
 import { ProductType } from '@/models'
+import { useProductsCart } from '@/hooks'
+import './product_card.sass'
 
-function ProductCard ({ products }: { products: ProductType[] }) {
-  const { isProductInCart, addProduct, removeProduct } = useProductsCart()
+interface PorductCartProps {
+  product: ProductType,
+}
+
+function ProductCard ({ product }: PorductCartProps) {
+  const { addProduct, isProductInCart, removeProduct  } = useProductsCart()
 
   return (
-    <>
-      {products.map(product => (
-        <Link key={product.id} className='not-decoration' to={`/products/${product.id}`}>
-          <article  className='product-card bg-primary' title={product.title}>
-            <img src={product.images[0]} alt={product.title} className='product-card__img' />
-            <header className='product-card__header'>
-              <h6 className='text-sm medium font-primary'>{product.title}</h6>
-              <p className='text-xs'>{product.category}</p>
-            </header>
-            <footer className='product-card__footer'>
-              <h3 className='text-2xl medium font-primary'>${product.price}</h3>
-              <ButtonCart
-                fontSize='text-sm'
-                isProductInCart={isProductInCart(product)}
-                addProduct={addProduct}
-                product={product}
-                removeProduct={removeProduct}
-              />
-            </footer>
-          </article>
-        </Link>
-      ))
-      }
-
-    </>
+    <article className='product-card bg-primary' title={product.title}>
+      <Link key={product.id} className='not-decoration' to={`/products/${product.id}`}>
+        <img src={product.images[0]} alt={product.title} className='product-card__img' />
+      </Link>
+      <header className='product-card__header'>
+        <h6 className='text-sm medium font-primary'>{product.title}</h6>
+        <p className='text-xs'>{product.category}</p>
+      </header>
+      <footer className='product-card__footer'>
+        <h3 className='text-2xl medium font-primary'>${product.price}</h3>
+        <ButtonCart
+          fontSize='text-sm'
+          isProductInCart={isProductInCart(product)}
+          addProduct={addProduct}
+          product={product}
+          removeProduct={removeProduct}
+        />
+      </footer>
+    </article>
   )
 }
 
