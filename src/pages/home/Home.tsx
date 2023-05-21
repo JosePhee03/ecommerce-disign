12701006c1072e7877f62a20b8a0fda38f2df9ce
Jose Luis Gutierrez <joselugz03@gmail.com>
2023-05-20 22:57:55 -0300

    ✅ Add QueryClientProvider

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,17 +1,23 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
 import { Header } from '@/components'
 
 import { FilterCont, ProductsCont, SearchCont } from './components'
 
 import './home.sass'
 
+const queryClient = new QueryClient()
+
 function Home () {
 
   return (
     <main className='home-layout'>
       <Header variant='home' text='Shop' />
-      <SearchCont />
-      <FilterCont />
-      <ProductsCont />
+      <QueryClientProvider client={queryClient}>
+        <SearchCont />
+        <FilterCont />
+        <ProductsCont />
+      </QueryClientProvider>
     </main>
   )
 }
